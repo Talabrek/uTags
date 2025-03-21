af1c0bf4a80320a18b7bce687a4955e1fc401700
Alexander Haffly <ahaffly@gmail.com>
2025-03-21 16:13:30 +0900

    Update Tag.java

--- src/main/java/com/blockworlds/utags/model/Tag.java
+++ src/main/java/com/blockworlds/utags/model/Tag.java
@@ -2,16 +2,23 @@ package com.blockworlds.utags.model;
 
 import org.bukkit.inventory.ItemStack;
 
+/**
+ * Represents a tag that can be displayed with a player's name in chat.
+ */
 public class Tag {
-    private String name;
-    private String display;
-    private TagType type;
-    private boolean isPublic;
-    private boolean color;
-    private int weight;
-    private ItemStack material;
+    private final String name;
+    private final String display;
+    private final TagType type;
+    private final boolean isPublic;
+    private final boolean color;
+    private ItemStack material; // Not final to allow changes
+    private int weight; // Not final to allow weight adjustments
 
-    public Tag(String name, String display, TagType type, boolean isPublic, boolean color, ItemStack material, int weight) {
+    /**
+     * Creates a new Tag with all properties.
+     */
+    public Tag(String name, String display, TagType type, boolean isPublic, 
+               boolean color, ItemStack material, int weight) {
         this.name = name;
         this.display = display;
         this.type = type;
@@ -21,13 +28,30 @@ public class Tag {
         this.weight = weight;
     }
 
+    /** Returns the internal name of the tag */
     public String getName() { return name; }
+    
+    /** Returns the display text of the tag */
     public String getDisplay() { return display; }
+    
+    /** Returns the type of the tag (PREFIX, SUFFIX, BOTH) */
     public TagType getType() { return type; }
+    
+    /** Returns whether the tag is publicly available */
     public boolean isPublic() { return isPublic; }
+    
+    /** Returns whether the tag supports color */
     public boolean isColor() { return color; }
+    
+    /** Returns the material used to represent this tag in menus */
     public ItemStack getMaterial() { return material; }
+    
+    /** Sets the material used to represent this tag */
     public void setMaterial(ItemStack material) { this.material = material; }
+    
+    /** Returns the weight of the tag (higher = higher priority) */
     public int getWeight() { return weight; }
+    
+    /** Sets the weight of the tag */
     public void setWeight(int weight) { this.weight = weight; }
 }
