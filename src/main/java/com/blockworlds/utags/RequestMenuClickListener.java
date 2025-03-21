a2036d8229198f544c32f08cf3e7b48be31d9e4a
Alexander Haffly <ahaffly@gmail.com>
2025-03-21 14:33:56 +0900

    Update RequestMenuClickListener.java

--- src/main/java/com/blockworlds/utags/RequestMenuClickListener.java
+++ src/main/java/com/blockworlds/utags/RequestMenuClickListener.java
@@ -1,5 +1,7 @@
 package com.blockworlds.utags;
 
+import com.blockworlds.utags.model.CustomTagRequest;
+import com.blockworlds.utags.utils.Utils;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,7 +11,6 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class RequestMenuClickListener implements Listener {
-
     private final uTags plugin;
 
     public RequestMenuClickListener(uTags plugin) {
@@ -20,47 +21,38 @@ public class RequestMenuClickListener implements Listener {
     public void onInventoryClick(InventoryClickEvent event) {
         Inventory inventory = event.getClickedInventory();
         
-        // Check if the click is valid and in the correct inventory
         if (inventory == null || !event.getView().getTitle().contains("Custom Tag Requests")) {
             return;
         }
 
-        // Cancel the click to prevent item movement
         event.setCancelled(true);
         
-        // Ensure that the event is triggered by a player
         if (!(event.getWhoClicked() instanceof Player)) {
             return;
         }
         Player player = (Player) event.getWhoClicked();
 
-        // Check if a valid item was clicked
         ItemStack clickedItem = event.getCurrentItem();
         if (clickedItem == null || !clickedItem.hasItemMeta() || !clickedItem.getItemMeta().hasDisplayName()) {
             return;
         }
 
-        // Extract player name from the clicked item
         String playerName = ChatColor.stripColor(clickedItem.getItemMeta().getDisplayName());
         CustomTagRequest request = plugin.getCustomTagRequestByPlayerName(playerName);
 
         if (request == null) {
-            player.sendMessage(ChatColor.RED + "An error occurred. Please try again.");
+            Utils.sendError(player, "An error occurred. Please try again.");
             return;
         }
 
-        // Handle left and right clicks
         if (event.isLeftClick()) {
-            // Accept the custom tag request
             plugin.acceptCustomTagRequest(request);
-            player.sendMessage(ChatColor.GREEN + "Custom tag request accepted.");
+            Utils.sendSuccess(player, "Custom tag request accepted.");
         } else if (event.isRightClick()) {
-            // Deny the custom tag request
             plugin.denyCustomTagRequest(request);
-            player.sendMessage(ChatColor.RED + "Custom tag request denied.");
+            Utils.sendSuccess(player, "Custom tag request denied.");
         }
 
-        // Update the requests menu
         plugin.openRequestsMenu(player);
     }
 }
