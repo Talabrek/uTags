0fcca2df1f418976821da5211b5ddc980b67a4c3
Alexander Haffly <ahaffly@gmail.com>
2025-03-21 16:13:08 +0900

    Update TagType.java

--- src/main/java/com/blockworlds/utags/model/TagType.java
+++ src/main/java/com/blockworlds/utags/model/TagType.java
@@ -1,5 +1,15 @@
 package com.blockworlds.utags.model;
 
+/**
+ * Enum representing the possible types of a tag.
+ */
 public enum TagType {
-    PREFIX, SUFFIX, BOTH
+    /** Tag appears before player name */
+    PREFIX,
+    
+    /** Tag appears after player name */
+    SUFFIX,
+    
+    /** Tag can be used as either prefix or suffix */
+    BOTH
 }
