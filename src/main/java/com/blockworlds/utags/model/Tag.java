7eeca7560d5ac1fb313a77695f240bae9bed1adf
Alexander Haffly <ahaffly@gmail.com>
2025-03-21 14:31:05 +0900

    Create Tag.java

---
+++ src/main/java/com/blockworlds/utags/model/Tag.java
@@ -0,0 +1,33 @@
+package com.blockworlds.utags.model;
+
+import org.bukkit.inventory.ItemStack;
+
+public class Tag {
+    private String name;
+    private String display;
+    private TagType type;
+    private boolean isPublic;
+    private boolean color;
+    private int weight;
+    private ItemStack material;
+
+    public Tag(String name, String display, TagType type, boolean isPublic, boolean color, ItemStack material, int weight) {
+        this.name = name;
+        this.display = display;
+        this.type = type;
+        this.isPublic = isPublic;
+        this.color = color;
+        this.material = material;
+        this.weight = weight;
+    }
+
+    public String getName() { return name; }
+    public String getDisplay() { return display; }
+    public TagType getType() { return type; }
+    public boolean isPublic() { return isPublic; }
+    public boolean isColor() { return color; }
+    public ItemStack getMaterial() { return material; }
+    public void setMaterial(ItemStack material) { this.material = material; }
+    public int getWeight() { return weight; }
+    public void setWeight(int weight) { this.weight = weight; }
+}
